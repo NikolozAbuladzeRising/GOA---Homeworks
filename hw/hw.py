196a06302299bf29f995625799a1a14da12a43da
NikolozAbuladzeRising <nikolozabuladzerising2026@gmail.com>
2026-08-13 11:45:51 +0400

    commit
    commit

---
+++ hw/hw.py
@@ -0,0 +1,70 @@
+x = 10
+y = "20"
+
+# სტრინგის გადაყვანა ინტეჯერად და შეკრება
+result = x + int(y)
+
+print(result)
+
+
+
+
+
+num_var = 12
+str_var = " წლის"
+
+# რიცხვის გადაქცევა სტრინგად და მათი შეერთება
+result = str(num_var) + str_var
+
+print(result)
+
+
+# შემოვატანინოთ რიცხვი მომხმარებელს (გარდავქმნათ მთელ რიცხვად - int)
+user_input = int(input("შემოიტანეთ რიცხვი: "))
+
+# შევქმნათ ცვლადი სხვა ციფრით
+my_number = 10
+
+# გამოვთვალოთ და დავპრინტოთ ჯამი
+total = user_input + my_number
+print("ჯამი არის:", total)
+
+
+x = "5"
+x = int(x)
+print(x)
+
+
+x = 6
+x = float (x)
+print(x)
+
+# ჩვეულებრივი გაყოფა (/ - აბრუნებს ათწილადს)
+rezultati_1 = 7 / 2
+print(rezultati_1)  
+
+# მთელ რიცხვამდე გაყოფა (// - ამრგვალებს ქვემოთ მთელ რიცხვამდე)
+rezultati_2 = 7 // 2
+print(rezultati_2) 
+
+# შემოვიტანოთ ასაკი
+asaki = input("შეიყვანეთ თქვენი ასაკი: ")
+
+# გადავყვანოთ რიცხვად (int) და მივუმატოთ 5
+samomavlo_asaki = int(asaki) + 5
+
+# დავბეჭდოთ შედეგი
+print(f"5 წლის შემდეგ თქვენ იქნებით {samomavlo_asaki} წლის.")
+
+
+
+name = input("ნიკოლოზი")
+surname = input("აბულაძე")
+
+print("გამარჯობა " + name + " " + surname)
+
+
+
+
+
+
