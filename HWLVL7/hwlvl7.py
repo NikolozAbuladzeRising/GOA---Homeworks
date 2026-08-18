47aec0e7dbd5907d52956103907ccd0dda997378
NikolozAbuladzeRising <nikolozabuladzerising2026@gmail.com>
2026-08-18 10:31:39 +0400

    Commit
    Commit

---
+++ HWLVL7/hwlvl7.py
@@ -0,0 +1,65 @@
+# Boolean არის მნიშვნელობა რომელიც იყენებს True Or False 
+# True არის სწორი,ჭეშმარიტი
+# False არის არასწორე , შეცდომა
+
+
+
+# შედარების ოპერაციები არის მეტობა ნაკლებობა ტოლობა მიმატება გამოკლება და ასე შემდეგ.
+# შედარების ოპერაციები გვეხმარება კოდი არის თუ არა სწორედ დაწერილი მაგალითად:
+
+number = 12
+number2 = 22
+print(number > number2) # ტერმინალში გამოაქვს False რადგანაც შეცდომაა და არასწორეა
+
+number = 50
+number2 = 250
+
+print(number < number2) # ამ შემთხვევაში Terminal ში გამოდის True ანუ ჭეშმარიტი , სწორედ დაწერილი კოდი
+
+# True მაგალითები:
+GOANUMBER1 = 50
+GOANUMBER2 = 25
+print(GOANUMBER1 > GOANUMBER2)
+
+
+GOANUMBER3 = 150
+GOANUMBER4 = 265
+print(GOANUMBER3 < GOANUMBER4)
+
+GOANUMBER5 = 300
+GOANUMBER6 = 250
+print(GOANUMBER5 > GOANUMBER6)
+
+GOANUMBER7 = 600
+GOANUMBER8 = 300
+print(GOANUMBER7 > GOANUMBER8)
+
+GOANUMBER9 = 642
+GOANUMBER10 = 460
+print(GOANUMBER9 > GOANUMBER10)
+
+# False მაგალითები:
+
+GOABESTACADEMY1 = 250
+GOABESTACADEMY2 = 500
+print(GOABESTACADEMY1 > GOABESTACADEMY2)
+
+GOABESTACADEMY3 = 300
+GOABESTACADEMY4 = 450
+print(GOABESTACADEMY3 > GOABESTACADEMY4)
+
+GOABESTACADEMY5 = 780
+GOABESTACADEMY6 = 464
+print(GOABESTACADEMY5 < GOABESTACADEMY6)
+
+GOABESTACADEMY7 = 950
+GOABESTACADEMY8 = 760
+print(GOABESTACADEMY7 < GOABESTACADEMY8)
+
+GOABESTACADEMY9 = 1050
+GOABESTACADEMY10 = 750
+print(GOABESTACADEMY9 < GOABESTACADEMY10)
+
+
+age = int(input("12"))
+print(age > 18)
