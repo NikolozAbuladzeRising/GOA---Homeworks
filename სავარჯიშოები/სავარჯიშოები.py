6f29fa9741b15e5bf70abc93daac12234912634d
NikolozAbuladzeRising <nikolozabuladzerising2026@gmail.com>
2026-09-12 13:51:19 +0400

    commit
    commit

---
+++ სავარჯიშოები/სავარჯიშოები.py
@@ -0,0 +1,57 @@
+# print("ნიკოლოზ აბულაძე")
+
+
+# name = "ნიკოლოზი"
+
+# name = input("შეიყვანე შენი სახელი")
+
+# print(name)
+
+# age = int (input("შეიყვანეთ თქვენი ასაკი"))
+
+# print(age)
+
+
+# price = float(input("შეიყვანეთ თქვენი ფასი"))
+
+# print(price)
+
+# num1 = int(input("შეიყვანეთ თქვენი რიცხვი"))
+# num2 = int(input("შეიყვანეთ თქვენი მეორე რიცხვი"))
+# print(num1 + num2)
+
+# num1 = int(input("შეიყვანეთ პირველი რიცხვი"))
+# num2 = int(input("შეიყვანეთ მეორე რიცხვი"))
+# print(num1 > num2)
+
+
+# age2 = int(input("შეიყვანე შენი ასაკი2"))
+
+# if age2 >= 18:
+#     print("Adult")
+
+# age3 = 12
+
+# if age3 <= 18:
+#     print("Teen")
+
+# elif age3 >= 18:
+#     print("Adult")
+
+# for i in range(1, 10):
+#     print(i)
+
+
+while x in range(1, 5):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
